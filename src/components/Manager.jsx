034a44624c640557c63dd0c45c6975251238c11f
Arya Sumant <studywithpwno.1@gmail.com>
2025-11-10 12:14:56 +0530

    auto update 2025-11-10 12:14:56

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -96,26 +96,26 @@ const Manager = () => {
             <table class="table-auto">
   <thead>
     <tr>
-      <th class="px-4 py-2">Title</th>
-      <th class="px-4 py-2">Author</th>
-      <th class="px-4 py-2">Views</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td class="border px-4 py-2">Intro to CSS</td>
-      <td class="border px-4 py-2">Adam</td>
-      <td class="border px-4 py-2">858</td>
-    </tr>
-    <tr class="bg-gray-100">
-      <td class="border px-4 py-2">A Long and Winding Tour of the History of UI Frameworks and Tools and the Impact on Design</td>
-      <td class="border px-4 py-2">Adam</td>
-      <td class="border px-4 py-2">112</td>
-    </tr>
-    <tr>
-      <td class="border px-4 py-2">Intro to JavaScript</td>
-      <td class="border px-4 py-2">Chris</td>
-      <td class="border px-4 py-2">1,280</td>
+      <th className="px-4 py-2">Title</th>
+      <th className="px-4 py-2">Author</th>
+      <th className="px-4 py-2">Views</th>
+    </tr>Name
+  </thead>Name
+  <tbody>Name
+    <tr>Name
+      <td className="border px-4 py-2">Intro to CSS</td>
+      <td className="border px-4 py-2">Adam</td>
+      <td className="border px-4 py-2">858</td>
+    </tr>Name
+    <tr class="Namebg-gray-100">
+      <td className="border px-4 py-2">A Long and Winding Tour of the History of UI Frameworks and Tools and the Impact on Design</td>
+      <td className="border px-4 py-2">Adam</td>
+      <td className="border px-4 py-2">112</td>
+    </tr>Name
+    <tr>Name
+      <td className="border px-4 py-2">Intro to JavaScript</td>
+      <td className="border px-4 py-2">Chris</td>
+      <td className="border px-4 py-2">1,280</td>
     </tr>
   </tbody>
 </table>
