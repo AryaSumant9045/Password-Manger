403677d38d21a4754d6df69d0e81c84a57fa1938
Arya Sumant <studywithpwno.1@gmail.com>
2025-11-10 14:10:32 +0530

    auto update 2025-11-10 14:10:32

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-slate-800 text-white flex ">
+    <div className="bg-slate-800 text-white flex flex-col justify-center items-center">
       <div>
         <h1 className="text-1xl text-center">
           <span className="text-green-600"> &lt;</span>
@@ -10,7 +10,7 @@ const Footer = () => {
           <span className="text-green-600"> OP/&gt;</span>
         </h1>
       </div>
-      <div>
+      <div className="flex">
         Created With <img src="/icons/love.png" alt="love" />
         by AryaSumant
       </div>
