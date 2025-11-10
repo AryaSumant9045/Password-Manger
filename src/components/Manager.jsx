b9b95d8f7ed1dff8893d143704c56a7a3fb5dc03
Arya Sumant <studywithpwno.1@gmail.com>
2025-11-10 16:29:51 +0530

    auto update 2025-11-10 16:29:51

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -35,8 +35,19 @@ const Manager = () => {
   };
 
   const copyText = (text) => {
-    alert("copied to clipbord" + text);
-    navigator.clipboard.writeText(text);
+    // alert("copied to clipbord" + text);
+    // navigator.clipboard.writeText(text);
+    toast("Copied to clickborad!", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      transition: Bounce,
+    });
   };
 
   return (
@@ -52,7 +63,7 @@ const Manager = () => {
         draggable
         pauseOnHover
         theme="light"
-        transition='Bounce'
+        transition="Bounce"
       />
       <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
         <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]"></div>
