8135ad5366e7849c61085e29b1d692db4710f73f
Arya Sumant <studywithpwno.1@gmail.com>
2025-11-10 20:48:17 +0530

    auto update 2025-11-10 20:48:17

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -73,7 +73,7 @@ const Manager = () => {
       pauseOnHover: true,
       draggable: true,
       progress: undefined,
-      theme: "light",
+      theme: "dark",
     });
     navigator.clipboard.writeText(text);
   };
