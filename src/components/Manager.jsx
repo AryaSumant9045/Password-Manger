aa483a869e660bd09c2b60f9d2e61f8edc60d85d
Arya Sumant <studywithpwno.1@gmail.com>
2025-11-10 16:49:45 +0530

    auto update 2025-11-10 16:49:45

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -197,7 +197,7 @@ const Manager = () => {
                       </div>
                     </td>
                     <td className="border px-4 py-2 text-center">
-                      <span><img src="/icons/edit_hover.web" alt="" /></span>
+                      <span><img className="w-4" src="/icons/icons8-edit.aep" alt="" /></span>
                     </td>
                   </tr>
                 );
