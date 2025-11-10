6b8b69cf13bd25879bcec261134916820ab0a46f
Arya Sumant <studywithpwno.1@gmail.com>
2025-11-10 18:36:38 +0530

    auto update 2025-11-10 18:36:38

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -43,7 +43,7 @@ const Manager = () => {
   const editPassword = (id) => {
       console.log("Editing password with id:" + id)
     // console.log(form)
-    // setPasswordArray([...passwordArray, form],{...form , id: uuidv4()});
+    setPasswordArray(passwordArray.filter(i=>i.id==id));
     // localStorage.setItem("passwords", JSON.stringify([...passwordArray, form]));
     // console.log(...passwordArray, form);
   };
