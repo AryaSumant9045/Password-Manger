0faf64da578260747567b6381f0b1fe8a678b8b7
Arya Sumant <studywithpwno.1@gmail.com>
2025-11-10 14:08:25 +0530

    auto update 2025-11-10 14:08:25

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
+    <div className="bg-slate-800">
       <div>
         <h1 className="text-1xl text-center">
           <span className="text-green-600"> &lt;</span>
