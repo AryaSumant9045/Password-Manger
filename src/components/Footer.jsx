cb4aa5e87c65d0111e1f3a086189cafb2fec3d82
Arya Sumant <studywithpwno.1@gmail.com>
2025-11-10 14:06:17 +0530

    auto update 2025-11-10 14:06:17

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
     <div>
       <div>
-
+        <h1 className="text-4xl font-bold text-center">
+          <span className="text-green-600"> &lt;</span>
+          Pass
+          <span className="text-green-600"> OP/&gt;</span>
+        </h1>
       </div>
       <div>
-        Created With <img src="/icons/love.png" alt="love" />by AryaSumant
+        Created With <img src="/icons/love.png" alt="love" />
+        by AryaSumant
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
