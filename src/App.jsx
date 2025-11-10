74ecc3044d94b070d966a5ca6f529448ffc336dc
Arya Sumant <studywithpwno.1@gmail.com>
2025-11-10 22:00:16 +0530

    auto update 2025-11-10 22:00:16

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
-import "./App.css";
-import Navbar from "./components/Navbar";
-import Manager from "./components/Manager";
-import Footer from "./components/Footer";
+
+import './App.css'
+import Navbar from './components/Navbar'
+import Manager from './components/Manager'
+import Footer from './components/Footer'
 function App() {
+  
+
   return (
     <>
-      <div className="className w-full sm:w-3/4 md:w-1/2 lg:w-1/3">
-        <Navbar />
-        {/* fixed to viewport — always visible as you scroll */}
-        <div
-          className="fixed inset-0 -z-10 w-full bg-white
+      <Navbar/>
+      {/* fixed to viewport — always visible as you scroll */}
+<div className="fixed inset-0 -z-10 w-full bg-white
   bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)]
-  bg-[size:6rem_4rem] bg-fixed"
-        >
-          <div className="absolute inset-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]"></div>
-        </div>
-        <Manager />
-        <Footer />
-        <div></div>
-      </div>
+  bg-[size:6rem_4rem] bg-fixed">
+  <div className="absolute inset-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]"></div>
+</div>
+      <Manager/>
+      <Footer/>
+    <div></div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
